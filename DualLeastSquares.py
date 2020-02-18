more fixes batching on gradient calc and prediction mnist example improvements

DualLeastSquares.py
@@ -1,5 +1,6 @@
 import Update, Error, Regularize, Activation, Initialize
 from DualNumber.DualArithmetic import DualGrad as Dual
+from Train import batch
 
 import numpy as np
 import pdb
@@ -43,27 +44,54 @@ class LLS( object ):
    
    def initialize_( self ):
       self.weight_ = self.iweight_( ( self.inputSize_, self.outputSize_ ), self.nParameters_ )
-
-   def partial_fit( self, X, Y ):
-      '''X and Y should both be 2d numpy arrays'''
-      error = self.error( X, Y )
-      grad = error.e_
-      while len( grad.shape ) > 1:
-         grad = np.average( grad, 1 )
-      grad = grad.reshape( self.weight_.x_.shape )
+   
+   def partial_fit( self, X, Y, maxBatch=None ):
+      '''Perform single round of training without reset. Returns average error.'''
+      grad, loss = np.zeros_like( self.weight_.x_ ), 0
+      for bx, by in batch( X, Y, maxBatch ):
+         portion = float( len( bx ) ) / len( X )
+         bgrad, bloss = self.trainGrad_( bx, by )
+         grad += bgrad * portion
+         loss = bloss * portion
+      grad += self.regularizeGrad_()
       self.weight_ = self.update_( self.weight_, grad )
-      return np.average( error.x_ )
+      return np.average( loss )
 
-   def predict( self, X ):
+   def predict( self, X, maxBatch=None ):
       '''X should be 2d numpy array'''
+      if maxBatch is None:
+         maxBatch = len( X )
       if X.shape[ 1 ] == self.inputSize_ - 1:
          X = np.append( np.ones( ( X.shape[ 0 ], 1 ) ), X, 1 )
-      raw = Dual( X, n=self.nParameters_ ).matmul( self.weight_ )
-      return self.outputActivation_( raw )
+      
+      yPred = Dual( np.zeros( ( len( X ), self.outputSize_ ) ), 0, self.weight_.n_ )
+      lower = 0
+      for bx in batch( X, maxBatch=maxBatch ):
+         raw = Dual( bx, n=self.nParameters_ ).matmul( self.weight_ )
+         upper = lower + len( bx )
+         yPred[ lower:upper ] = self.outputActivation_( raw )
+         lower += maxBatch
+      return yPred
    
    def error( self, X, Y ):
       '''X and y should both be 2d numpy arrays'''
       return self.errorFunc_( Y, self.predict( X ) )
+
+   def regularizeGrad_( self ):
+      '''return gradient for regularization of weights in same shape as weights'''
+      grad = self.regLoss_( self.weight_ ).e_
+      while len( grad.shape ) > 1:
+         grad = np.sum( grad, 1 )
+      return grad.reshape( self.weight_.x_.shape )
+   
+   def trainGrad_( self, X, Y ):
+      '''return gradient for training weights in same shape as weights as well as the average loss'''
+      loss = self.error( X, Y )
+      grad = loss.e_
+      grad = np.average( grad, 1 ) # across samples
+      while len( grad.shape ) > 1:
+         grad = np.sum( grad, 1 ) # across outputs
+      return grad.reshape( self.weight_.x_.shape ), np.average( loss.x_ )
 
    def cool( self ):
       self.updateObj_.cool()

DualNumber/DualArithmetic.py
@@ -149,6 +149,8 @@ class DualNumber( object ):
         return self.x_ > other
     
     # utilities
+    def __len__( self ):
+        return len( self.x_ )
     def where( self, condition, other ):
         '''where condition is true, substitute from other into self for return'''
         if isinstance( other, DualNumber ):
@@ -164,11 +166,11 @@ class DualNumpy( DualNumber ):
     def __init__( self, x=0, e=None ):
         self.x_ = np.array( x )
         if e is None:
-            self.e_ = np.zeros_like( x )
+            self.e_ = np.zeros_like( x, dtype=float )
         else:
-            self.e_ = np.array( e )
+            self.e_ = np.array( e, dtype=float )
             if not self.e_.shape:
-                self.e_ = np.full( self.x_.shape, e )
+                self.e_ = np.full( self.x_.shape, e, dtype=float )
         assert self.x_.shape == self.e_.shape, 'size mismatch'
     def __str__( self ):
         return "{} + ({})e".format( self.x_, self.e_ )
@@ -198,7 +200,7 @@ class DualNumpy( DualNumber ):
             self.x_[ key ] = value.x_
             self.e_[ key ] = value.e_
         self.x_[ key ] = value
-        self.e_[ key ] = np.zeros_like( self.e_[ key ] )
+        self.e_[ key ] = np.zeros_like( self.e_[ key ], dtype=float )
     def where( self, condition, other ):
         '''where condition is true, substitute from other into self for return '''
         if isinstance( other, DualNumber ):
@@ -224,13 +226,13 @@ class DualGrad( DualNumpy ):
         '''n = number of independant variables to represent in e
            e will have one dimension more than x'''
         self.n_ = n
-        self.x_ = np.array( x )
+        self.x_ = np.array( x, dtype=float  )
         if e is None:
-            self.e_ = np.zeros( ( n, *self.x_.shape ) )
+            self.e_ = np.zeros( ( n, *self.x_.shape ), dtype=float  )
         else:
-            self.e_ = np.array( e )
+            self.e_ = np.array( e, dtype=float  )
             if not self.e_.shape:
-                self.e_ = np.full( ( n, *self.x_.shape ), e )
+                self.e_ = np.full( ( n, *self.x_.shape ), e, dtype=float  )
 
         # can't realign where e is a different size, so just assert
         assert ( n, *self.x_.shape ) == self.e_.shape, 'size mismatch'
@@ -310,18 +312,19 @@ class DualGrad( DualNumpy ):
     
     # utilities
     def __getitem__( self, key ):
+        eKey = ( slice( None ), key ) if not isinstance( key, tuple ) else ( slice(None), *key )
         ret = type(self)( 0, 0, self.n_ )
         ret.x_ = self.x_[ key ]
-        ret.e_ = self.e_[ ( slice(None), *key ) ]
+        ret.e_ = self.e_[ eKey ]
         return ret
     def __setitem__( self, key, value ):
-        eKey = ( slice(None), *key )
+        eKey = ( slice( None ), key ) if not isinstance( key, tuple ) else ( slice(None), *key )
         if isinstance( value, DualGrad ):
             self.x_[ key ] = value.x_
             self.e_[ eKey ] = value.e_
             return
         self.x_[ key ] = value
-        self.e_[ eKey ] = np.zeros_like( self.e_[ eKey ] )
+        self.e_[ eKey ] = np.zeros_like( self.e_[ eKey ], dtype=float )
     def sum( self, axis ):
         eAxis = axis if axis < 0 else axis + 1
         return type(self)( np.sum( self.x_, axis ), np.sum( self.e_, eAxis ), self.n_ )

DualSuperConnect.py
@@ -1,6 +1,7 @@
 
 import Update, Error, Regularize, Activation, Initialize
 from DualNumber.DualArithmetic import DualGrad as Dual
+from Train import batch
 
 import numpy as np
 from sklearn.metrics import mean_squared_error
@@ -62,26 +63,42 @@ class SCNN( object ):
       self.weight_ = self.iweight_( ( self.valueInSize_, self.valueOutSize_ ), self.nParameters_)
       self.valueOut_ = None
    
-   def partial_fit( self, X, Y ):
-      '''Perform single round of training without reset. Returns average error.'''
-      error = self.error( X, Y )
-      grad = np.average( error.e_, 1 ).reshape( self.weight_.x_.shape )
+   def partial_fit( self, X, Y, maxBatch=None, elide=[] ):
+      '''Perform single round of training without reset. Returns average error.
+         max_batch indicates the number of sample from X to run at once. Exactly one update step is taken.'''
+      grad, loss = np.zeros_like( self.weight_.x_ ), 0
+      for bx, by in batch( X, Y, maxBatch ):
+         portion = float( len( bx ) ) / len( X )
+         bgrad, bloss = self.trainGrad_( bx, by, elide )
+         grad += bgrad * portion
+         loss = bloss * portion
+      grad += self.regularizeGrad_()
       self.weight_ = self.update_( self.weight_, grad )
-      return np.average( error.x_ )
+      return np.average( loss )
 
-   def predict( self, X, elide=[] ):
+   def predict( self, X, maxBatch=None, elide=[] ):
       '''Perform forward pass.
-      idempotent only if independent specified True.
+      idempotent when recurrent=False.
       use elide list to leave out nodes (zero out, no update)'''
-
-      if not isinstance( elide, list ):
-         elide = [ elide ]
+      if maxBatch is None:
+         maxBatch = len( X )
       if X.shape[ 1 ] == self.inputSize_ - 1:
          X = np.append( np.ones( ( X.shape[ 0 ], 1 ) ), X, 1 )
+      if not isinstance( elide, list ):
+         elide = [ elide ]
 
-      if self.recurrent_:
-         return self.predictRecurrent_( X, elide )
-      return self.predictIndependent_( X, elide )
+      yPred = Dual( np.zeros( ( len( X ), self.outputSize_ ) ), 0, self.weight_.n_ )
+      lower = 0
+      by = None
+      for bx in batch( X ):
+         if self.recurrent_:
+            by = self.predictRecurrent_( bx, elide )
+         else:
+            by = self.predictIndependent_( bx, elide )
+         upper = lower + len( bx )
+         yPred[ lower:upper ] = by
+         lower += maxBatch
+      return yPred
    
    def predictIndependent_( self, X, elide ):
       self.valueOut_ = Dual( np.zeros( ( len( X ), self.valueOutSize_ ) ), 0, self.nParameters_ )
@@ -89,6 +106,8 @@ class SCNN( object ):
          X = np.append( X, np.zeros( ( X.shape[ 0 ], self.valueInSize_ - X.shape[ 1 ] ) ), 1 )
       self.valueIn_ = Dual( X, 0, self.nParameters_ )
       for n in range( self.valueOutSize_ ):
+         if n in elide:
+            continue
          m = n + 1
          self.valueOut_[ :, n:m] = self.valueIn_.matmul( self.weight_[ :, n:m ] )
          if n < self.hiddenSize_:
@@ -104,48 +123,67 @@ class SCNN( object ):
       for s in range( len( X ) ):
          self.valueIn_[ :self.inputSize_ ] = X[ s ]
          for n in range( self.valueOutSize_ ):
+            if n in elide:
+               continue
             m = n + 1
             self.valueOut_[ n:m ] = self.valueIn_.matmul( self.weight_[ :, n:m ] )
             self.valueIn_[ self.inputSize_ + n:self.inputSize_ + m ] = self.hiddenActivation_( self.valueOut_[ n:m ] )
          pred[ s ] = self.outputActivation_( self.valueOut_[ self.hiddenSize_: ] )
       return pred
 
-   def error( self, X, Y, elide=[] ):
+   def error( self, X, Y, maxBatch=None, elide=[] ):
       '''X and y should both be 2d numpy arrays'''
-      return self.errorFunc_( Y, self.predict( X, elide ) )
+      pred = self.predict( X, elide=elide )
+      return self.errorFunc_( Y, pred )
 
-   def addNode( self ):
-      self.valueIn_ = np.insert( self.valueIn_, self.inputSize_, 0 )
-      self.oldValueIn_ = self.valueIn_.copy()
-      self.valueOut_ = np.insert( self.valueOut_, 0, 0 )
-      self.dValueOut_ = np.insert( self.dValueOut_, 0, 0 )
-      self.weight_ = np.insert( self.weight_, 0, np.random.normal( scale=np.sqrt( self.valueInSize_ ),
-                                                                   size=self.valueInSize_ ), axis=0 )
-      self.weight_ = np.insert( self.weight_, self.inputSize_, 0, axis=1 ) # np.random.normal( size=self.valueOutSize_ + 1 ), axis=1 )
-      self.dWeight_ = np.insert( self.dWeight_, 0, 0, axis=0 )
-      self.dWeight_ = np.insert( self.dWeight_, self.inputSize_, 0, axis=1 )
-      self.hiddenSize_ += 1
-
-      self.valueInSize_ = self.inputSize_ + self.hiddenSize_
-      self.valueOutSize_ = self.hiddenSize_ + self.outputSize_
-
-      self.updateObj_.add( 0, self.inputSize_ )
+   def regularizeGrad_( self ):
+      '''return gradient for regularization of weights in same shape as weights'''
+      grad = self.regLoss_( self.weight_ ).e_
+      while len( grad.shape ) > 1:
+         grad = np.sum( grad, 1 )
+      return grad.reshape( self.weight_.x_.shape )
    
-   def removeNode( self, index ):
-      self.valueIn_ = np.delete( self.valueIn_, self.inputSize_ + index )
-      self.oldValueIn_ = self.valueIn_.copy()
-      self.valueOut_ = np.delete( self.valueOut_, index )
-      self.dValueOut_ = np.delete( self.dValueOut_, index )
-      self.weight_ = np.delete( self.weight_, index, axis=0 )
-      self.weight_ = np.delete( self.weight_, self.inputSize_ + index, axis=1 )
-      self.dWeight_ = np.delete( self.dWeight_, index, axis=0 )
-      self.dWeight_ = np.delete( self.dWeight_, self.inputSize_ + index, axis=1 )
-      self.hiddenSize_ -= 1
+   def trainGrad_( self, X, Y, elide ):
+      '''return gradient for training weights in same shape as weights as well as the average loss'''
+      loss = self.error( X, Y, elide=elide )
+      grad = loss.e_
+      grad = np.average( grad, 1 ) # across samples
+      while len( grad.shape ) > 1:
+         grad = np.sum( grad, 1 ) # across outputs
+      return grad.reshape( self.weight_.x_.shape ), np.average( loss.x_ )
 
-      self.valueInSize_ = self.inputSize_ + self.hiddenSize_
-      self.valueOutSize_ = self.hiddenSize_ + self.outputSize_
+   # def addNode( self ):
+   #    self.valueIn_ = np.insert( self.valueIn_, self.inputSize_, 0 )
+   #    self.oldValueIn_ = self.valueIn_.copy()
+   #    self.valueOut_ = np.insert( self.valueOut_, 0, 0 )
+   #    self.dValueOut_ = np.insert( self.dValueOut_, 0, 0 )
+   #    self.weight_ = np.insert( self.weight_, 0, np.random.normal( scale=np.sqrt( self.valueInSize_ ),
+   #                                                                 size=self.valueInSize_ ), axis=0 )
+   #    self.weight_ = np.insert( self.weight_, self.inputSize_, 0, axis=1 ) # np.random.normal( size=self.valueOutSize_ + 1 ), axis=1 )
+   #    self.dWeight_ = np.insert( self.dWeight_, 0, 0, axis=0 )
+   #    self.dWeight_ = np.insert( self.dWeight_, self.inputSize_, 0, axis=1 )
+   #    self.hiddenSize_ += 1
 
-      self.updateObj_.remove( index, self.inputSize_ + index )
+   #    self.valueInSize_ = self.inputSize_ + self.hiddenSize_
+   #    self.valueOutSize_ = self.hiddenSize_ + self.outputSize_
+
+   #    self.updateObj_.add( 0, self.inputSize_ )
+   
+   # def removeNode( self, index ):
+   #    self.valueIn_ = np.delete( self.valueIn_, self.inputSize_ + index )
+   #    self.oldValueIn_ = self.valueIn_.copy()
+   #    self.valueOut_ = np.delete( self.valueOut_, index )
+   #    self.dValueOut_ = np.delete( self.dValueOut_, index )
+   #    self.weight_ = np.delete( self.weight_, index, axis=0 )
+   #    self.weight_ = np.delete( self.weight_, self.inputSize_ + index, axis=1 )
+   #    self.dWeight_ = np.delete( self.dWeight_, index, axis=0 )
+   #    self.dWeight_ = np.delete( self.dWeight_, self.inputSize_ + index, axis=1 )
+   #    self.hiddenSize_ -= 1
+
+   #    self.valueInSize_ = self.inputSize_ + self.hiddenSize_
+   #    self.valueOutSize_ = self.hiddenSize_ + self.outputSize_
+
+   #    self.updateObj_.remove( index, self.inputSize_ + index )
 
    def cool( self ):
       self.updateObj_.cool()

Example_Mnist.py
@@ -1,4 +1,4 @@
-import Activation, Error, Update
+import Activation, Error, Regularize, Update
 from DataFetch import fetch
 from DualNumber.TestLib import runTest
 from DualLeastSquares import LLS
@@ -18,8 +18,10 @@ import pdb
 np.seterr(invalid='raise')
 
 # Data
-n_components = 100
-train_samples = 5000 # max 70000; Memory error at 5000, need to do some batching to make it work
+n_components = 64
+train_samples = 50000 # max 70000
+batch = 100
+iters = 32
 X, y = fetch( 'mnist', lambda : fetch_openml( 'mnist_784', version=1, return_X_y=True ) )
 print( 'Data Fetch Complete' )
 random_state = check_random_state( 0 )
@@ -36,27 +38,23 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform( pca.fit_transform( X_train ) )
 X_test = scaler.transform( pca.transform( X_test ) )
 Y_train, Y_test = fct( y_train ), fct( y_test )
+print( 'Preprocessing Complete' )
 
-def linearMnist():
-   model = LLS( n_components, Y_train.shape[ 1 ],
-                outputAct=Activation.Softmax(),
-                update=Update.Rprop(),
-                error=Error.JsDivergence() )
-   iters = 10
+def testMnist( model ):
    loss = np.zeros( iters )
    train_acc = np.zeros( iters + 1 )
    test_acc = np.zeros( iters + 1 )
    for i in range( iters ):
-      raw_pred = model.predict( X_train ).x_
-      train_acc[ i ] = np.average( np.argmax( raw_pred, axis=1 ) == y_train )
-      raw_pred = model.predict( X_test ).x_
-      test_acc[ i ] = np.average( np.argmax( raw_pred, axis=1 ) == y_test )
-      loss[ i ] = model.partial_fit( X_train, Y_train )
+      raw_pred = model.predict( X_train, maxBatch=batch ).x_
+      train_acc[ i ] = np.average( np.argmax( raw_pred, axis=1 ) != y_train )
+      raw_pred = model.predict( X_test, maxBatch=batch ).x_
+      test_acc[ i ] = np.average( np.argmax( raw_pred, axis=1 ) != y_test )
+      loss[ i ] = model.partial_fit( X_train, Y_train, maxBatch=batch )
       print( i + 1, 'complete' )
-   raw_pred = model.predict( X_train ).x_
-   train_acc[ -1 ] = np.average( np.argmax( raw_pred, axis=1 ) == y_train )
-   raw_pred = model.predict( X_test ).x_
-   test_acc[ -1 ] = np.average( np.argmax( raw_pred, axis=1 ) == y_test )
+   raw_pred = model.predict( X_train, maxBatch=batch ).x_
+   train_acc[ -1 ] = np.average( np.argmax( raw_pred, axis=1 ) != y_train )
+   raw_pred = model.predict( X_test, maxBatch=batch ).x_
+   test_acc[ -1 ] = np.average( np.argmax( raw_pred, axis=1 ) != y_test )
    
    # Plot training results
    plt.figure()
@@ -66,15 +64,30 @@ def linearMnist():
 
    plt.title( 'Accuracy Curves' )
    plt.semilogy( train_acc, label='train' )
-   plt.semilogy( test_acc, lable='test')
+   plt.semilogy( test_acc, label='test')
    plt.legend()
    plt.show()
 
    pdb.set_trace()
 
+def linearMnist():
+   model = LLS( n_components, 10,
+                outputAct=Activation.Softmax(),
+                update=Update.Rprop(),
+                error=Error.JsDivergence(),
+                regularization=Regularize.Ridge() )
+   testMnist( model )
+
 def scnnMnist():
-   pass
+   model = SCNN( n_components, 10,
+                 hiddenSize=4,
+                 hiddenAct=Activation.Elu(),
+                 outputAct=Activation.Softmax(),
+                 update=Update.Rprop(),
+                 error=Error.JsDivergence(),
+                 regularization=Regularize.Ridge() )
+   testMnist( model )
 
 if __name__  == "__main__":
-   runTest( linearMnist )
-   # runTest( scnnMnist )
+   #runTest( linearMnist )
+   runTest( scnnMnist )